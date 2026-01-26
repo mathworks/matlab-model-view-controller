73fddbc03731b5abb154cc194af96ce9b0fcfd96
Ken Deeley <kdeeley@mathworks.com>
2026-01-26 11:59:36 +0000

    Added rng control

--- tests/Testable.m
+++ tests/Testable.m
@@ -2,5 +2,17 @@ classdef Testable < matlab.uitest.TestCase
     %TESTABLE Convenience class used for testing purposes.
 
     % Copyright 2025 The MathWorks, Inc.
+
+    methods ( TestClassSetup )
+
+        function setRNGState( testCase )
+
+            s = rng();
+            testCase.addTeardown( @() rng( s ) )            
+            rng( "default" )
+
+        end % setRNGState
+
+    end % methods ( TestClassSetup )
        
 end % classdef
